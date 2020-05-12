963e6ad8c656cbe5fd1d55c1bf9d2617a0368ea4
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-05-12 18:48:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import store from 'store';
+import store from 'redux/store';
 import App from 'components/App';
 import Firebase, { FirebaseContext } from 'components/firebase';
 
