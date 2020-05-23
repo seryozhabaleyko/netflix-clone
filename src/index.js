738d619cce6279fee97c580b7e94d198968aaebd
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-05-23 15:35:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,15 +4,44 @@ import { Provider } from 'react-redux';
 
 import store from 'store';
 import App from 'components/App';
-import Firebase, { FirebaseContext } from 'components/firebase';
+
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+};
+
+const rrfConfig = {
+    userProfile: 'users',
+    useFirestoreForProfile: true,
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+
+const rrfProps = {
+    firebase,
+    config: rrfConfig,
+    dispatch: store.dispatch,
+    createFirestoreInstance,
+};
 
 ReactDOM.render(
     <React.StrictMode>
-        <FirebaseContext.Provider value={new Firebase()}>
-            <Provider store={store}>
+        <Provider store={store}>
+            <ReactReduxFirebaseProvider {...rrfProps}>
                 <App />
-            </Provider>
-        </FirebaseContext.Provider>
+            </ReactReduxFirebaseProvider>
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
